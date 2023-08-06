4fbd5980130d14b23ca0748ee86ae1ae4d668ad3
httpsdfaa <deivisondfaa@gmail.com>
2023-08-06 19:42:18 -0300

    Melhoras das funções

--- lista_compras.py
+++ lista_compras.py
@@ -1,42 +1,52 @@
- # VARIÁVEIS
-lista_compras = []
+# VARIÁVEIS
+lista_compras_var = []
 
 # FUNÇÕES
 def VERIFICAR_TAMANHO_LETRA(minusc, maiusc, entrada):
     return entrada.lower() == minusc or entrada.upper() == maiusc
 
-def LISTA_COMPRAS():
-    if lista_compras == []: 
+def lista_compras():
+    if not lista_compras_var: 
         print('--SUA LISTA ESTÁ VAZIA--\n\n')
     else:
         print('LISTA DE COMPRAS\n')
-        for indice, item in enumerate(lista_compras):
-            print(f'{indice}: {item}')
+        for indice, item in enumerate(lista_compras_var):
+            print(f'{indice+1}: {item}')
 
 
     entrada_lista = input('\nVocê deseja adicionar item a lista? "S" ou  "N"->') # AÇÃO SE USUÁRIO DESEJA OU NÃO ADICIONAR ALGO A LISTA
     if VERIFICAR_TAMANHO_LETRA('s', 'S', entrada_lista):
         print('LISTA DE COMPRAS\n\n')
+        for indice, item in enumerate(lista_compras_var):
+            print(f'{indice+1}: {item}')
+
         INSERIR_COMPRAS()
     elif VERIFICAR_TAMANHO_LETRA('n', 'N', entrada_lista):
         main() # CHAMADA DA FUNÇÃO PRIMÁRIA PARA RECOMEÇAR A LÓGICA
-    elif lista_compras == []:
+    elif not lista_compras_var:
         print('LISTAS DE COMPRAS ESTÁ VAZIO\n')
-        LISTA_COMPRAS()
+        lista_compras()
     else:
         print('\nESCOLHA UMA OPÇÃO VÁLIDA\n')
         
 def INSERIR_COMPRAS():
-    i = 1
+    i = len(lista_compras_var) + 1
+    print('Para sair digite [sair, SAIR]')
     while True:
-        adicionar_valor_lista = input(f'{i}: ')
-        if VERIFICAR_TAMANHO_LETRA('sair', 'SAIR', adicionar_valor_lista):
-            return LISTA_COMPRAS()
+        adicionar_item_lista = input(f'{i}: ')
+        if VERIFICAR_TAMANHO_LETRA('sair', 'SAIR', adicionar_item_lista):
+            break
+        if adicionar_item_lista == '':
+            print('--A lista não pode ficar sem algum item--')
+            continue
         else:
-            lista_compras.append(adicionar_valor_lista)
+            lista_compras_var.append(adicionar_item_lista)
 
             i += 1
-            continue
+
+def DELETAR_COMPRAS():
+    ...
+
 def main():
     while True:
         entrada_in = input('L: Lista de compras \nI: Inserir item à lista de compras \n\
@@ -44,7 +54,10 @@ D: Apagar item da lista de compras \n-> ')
         nao_digito = not entrada_in.isdigit() # NÃO ACEITA SOMENTE NÚMEROS.
         
         if nao_digito and VERIFICAR_TAMANHO_LETRA('l', 'L', entrada_in):
-            LISTA_COMPRAS()
+            lista_compras()
         elif VERIFICAR_TAMANHO_LETRA('fechar', 'FECHAR', entrada_in):
+            print('encerrando o programa')
             break
+        else:
+            ...
 main()
