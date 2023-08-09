10f1459451e9743877fba68cbe52b430b28ac27a
httpsdfaa <deivisondfaa@gmail.com>
2023-08-09 11:47:20 -0300

    refatorando

--- lista_compras.py
+++ lista_compras.py
@@ -5,6 +5,10 @@ lista_compras_var = []
 def verificar_tamanho_letra(minusc, maiusc, entrada):
     return entrada.lower() == minusc or entrada.upper() == maiusc
 
+def saida_usuario():
+    ...
+
+
 def lista_compras():
     if not lista_compras_var: 
         print('--SUA LISTA ESTÁ VAZIA--\n\n')
@@ -12,7 +16,6 @@ def lista_compras():
         print('LISTA DE COMPRAS\n')
         for indice, item in enumerate(lista_compras_var):
             print(f'{indice+1}: {item}')
-
 
     entrada_lista = input('\nVocê deseja adicionar item a lista? [S/N]') # AÇÃO SE USUÁRIO DESEJA OU NÃO ADICIONAR ALGO A LISTA
     if verificar_tamanho_letra('s', 'S', entrada_lista):
@@ -23,20 +26,19 @@ def lista_compras():
         inserir_compras()
     elif verificar_tamanho_letra('n', 'N', entrada_lista):
         main() # CHAMADA DA FUNÇÃO PRIMÁRIA PARA RECOMEÇAR A LÓGICA
-    elif not lista_compras_var:
-        print('LISTAS DE COMPRAS ESTÁ VAZIO\n')
-        lista_compras()
     else:
         print('\nESCOLHA UMA OPÇÃO VÁLIDA\n')
-        
+        lista_compras()
+
+
 def inserir_compras():
     i = len(lista_compras_var) + 1
-    print('Para sair digite [sair, SAIR]')
+    print('Para sair digite [sair]')
     while True:
         adicionar_item_lista = input(f'{i}: ')
         if verificar_tamanho_letra('sair', 'SAIR', adicionar_item_lista):
             return lista_compras() 
-        elif adicionar_item_lista == '':
+        elif not adicionar_item_lista:
             print('--A lista não pode ficar sem algum item--')
             continue
         else:
@@ -44,8 +46,9 @@ def inserir_compras():
 
             i += 1
 
+
 def deletar_item():
-    if lista_compras_var == []:
+    if not lista_compras_var:
         print('--Sua lista de compras está vazio--')
         entrada_lista_2 = input('\nDeseja adicionar algo a lista? [S/N]')
         if verificar_tamanho_letra('s', 'S', entrada_lista_2):
@@ -53,15 +56,13 @@ def deletar_item():
         elif verificar_tamanho_letra('n', 'N', entrada_lista_2):
             return lista_compras()
         else:
-            print('Entrada inválida. Digite S ou N para continuar')
+            print('--Entrada inválida. Digite [S/N] para continuar--')
     else:
         for i, item in enumerate(lista_compras_var):
             print(f'{i+1}: {item}')
 
         while True:
             entrada_deletar = input('Digite o índice para deletar ou [S] para sair: ')
-            for i, item in enumerate(lista_compras_var):
-                print(f'{i+1}: {item}')
             
             if entrada_deletar.isdigit():
                 try:
@@ -72,6 +73,9 @@ def deletar_item():
                 except:
                     print('--Índice não existente--')
                     continue
+                
+                for i, item in enumerate(lista_compras_var):
+                    print(f'{i+1}: {item}')
 
             elif verificar_tamanho_letra('s', 'S', entrada_deletar):
                 return lista_compras()
@@ -97,5 +101,5 @@ D: Apagar item da lista de compras \n-> ')
             print('encerrando o programa')
             break
         else:
-            print('Entrada inválida.')
+            print('--Entrada inválida--')
 main()
