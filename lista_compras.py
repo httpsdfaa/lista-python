6fbc7eba502eb2daae882f0732d0e99dbbe0f875
httpsdfaa <deivisondfaa@gmail.com>
2023-08-10 13:10:18 -0300

    Implementando função atualizar

--- lista_compras.py
+++ lista_compras.py
@@ -5,21 +5,26 @@ lista_compras_var = []
 def verificar_tamanho_letra(minusc, maiusc, entrada):
     return entrada.lower() == minusc or entrada.upper() == maiusc
 
-def saida_usuario():
-    ...
+def prints_repetitivos(lista_vazia, entrada_invalida, lista_compras):
+    if lista_vazia == True:
+        print('--SUA LISTA ESTÁ VAZIA--\n\n')
+    elif entrada_invalida == True:
+        print('\nENTRADA INVÁLIDA\n')
+    elif lista_compras == True:
+        print('LISTA DE COMPRAS\n\n')
 
 
 def lista_compras():
     if not lista_compras_var: 
-        print('--SUA LISTA ESTÁ VAZIA--\n\n')
+        prints_repetitivos(True, False, False)
     else:
-        print('LISTA DE COMPRAS\n')
+        prints_repetitivos(False, False, True)
         for indice, item in enumerate(lista_compras_var):
             print(f'{indice+1}: {item}')
 
     entrada_lista = input('\nVocê deseja adicionar item a lista? [S/N]') # AÇÃO SE USUÁRIO DESEJA OU NÃO ADICIONAR ALGO A LISTA
     if verificar_tamanho_letra('s', 'S', entrada_lista):
-        print('LISTA DE COMPRAS\n\n')
+        prints_repetitivos(False, False, True)
         for indice, item in enumerate(lista_compras_var):
             print(f'{indice+1}: {item}')
 
@@ -27,7 +32,7 @@ def lista_compras():
     elif verificar_tamanho_letra('n', 'N', entrada_lista):
         main() # CHAMADA DA FUNÇÃO PRIMÁRIA PARA RECOMEÇAR A LÓGICA
     else:
-        print('\nESCOLHA UMA OPÇÃO VÁLIDA\n')
+        prints_repetitivos(False, True, False)
         lista_compras()
 
 
@@ -82,14 +87,55 @@ def deletar_item():
             elif verificar_tamanho_letra('n', 'N', entrada_deletar):
                 continue
             else:
-                print('--Entrada inválida--')
+                prints_repetitivos(False, True, False)
                 continue
             
+
+def atualizar_lista():
+    if not lista_compras_var:
+        prints_repetitivos(True, False, False)
+        entrada_atualizar = input('\nVocê deseja adicionar item a lista? [S/N]')
+        if verificar_tamanho_letra('s', 'S', entrada_atualizar):
+            return inserir_compras()
+        elif verificar_tamanho_letra('n', 'N', entrada_atualizar):
+            return main()
+        else:
+            prints_repetitivos(False, True, False)
+            main()
+    else:
+        for i, item in enumerate(lista_compras_var):
+            print(f'{i+1}: {item}')
+        
+        while True:
+            entrada_indice = input('Digite o índice que deseja atualizar ou [S] para sair: ')
+            if verificar_tamanho_letra('s', 'S', entrada_indice):
+                return main()
+            elif verificar_tamanho_letra('n', 'N', entrada_indice):
+                continue
+            
+            entrada_atualizar = input('Digite o novo item: ')
+
+            if entrada_indice.isdigit():
+                try:
+                    entrada_indice = int(entrada_indice)
+                    entrada_indice -= 1
+
+                    lista_compras_var[entrada_indice] = entrada_atualizar
+
+                except:
+                    print('--Índice não existente--')
+                    continue
+                
+                for i, item in enumerate(lista_compras_var):
+                    print(f'{i+1}: {item}')
+            else:
+                prints_repetitivos(False, True, False)
+                continue
 
 def main():
     while True:
         entrada_in = input('L: Lista de compras \nI: Inserir item à lista de compras \n\
-D: Apagar item da lista de compras \n-> ')
+D: Apagar item da lista de compras \nA: Atualizar item da lista \n-> ')
         
         if verificar_tamanho_letra('l', 'L', entrada_in): # LISTAR
             return lista_compras()
@@ -97,9 +143,11 @@ D: Apagar item da lista de compras \n-> ')
             return inserir_compras()
         elif verificar_tamanho_letra('d', 'D', entrada_in): # DELETAR
             return deletar_item()
+        elif verificar_tamanho_letra('a', 'A', entrada_in):
+            return atualizar_lista()
         elif verificar_tamanho_letra('fechar', 'FECHAR', entrada_in):
             print('encerrando o programa')
             break
         else:
-            print('--Entrada inválida--')
+            prints_repetitivos(False, True, False)
 main()
